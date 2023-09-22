39408b9d3a977b0fa1fa851382df8248bd272e2c
Nikita Kumari <37824543+nikitakk3101@users.noreply.github.com>
2023-09-22 13:32:36 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,10 @@
-# hello.py
-print("Hello, World test!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
